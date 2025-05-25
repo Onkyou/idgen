c2894dbf7acec7851088779e2c7d0c52c5791cf1
Onkyou <christmarvin@me.com>
2025-05-25 21:33:21 +0200

    added points of interest

--- pkg/idprefix/prefixes.go
+++ pkg/idprefix/prefixes.go
@@ -1,17 +1,19 @@
 package idprefix
 
 const (
-	Achievement = "ach"
-	Event       = "evt"
-	Message     = "msg"
-	Transaction = "txn"
-	User        = "usr"
+	Achievement     = "ach"
+	Event           = "evt"
+	Message         = "msg"
+	PointOfInterest = "poi"
+	Transaction     = "txn"
+	User            = "usr"
 )
 
 var PrefixToEntity = map[string]string{
-	Achievement: "Achievement",
-	Event:       "Event",
-	Message:     "Message",
-	Transaction: "Transaction",
-	User:        "User",
+	Achievement:     "Achievement",
+	Event:           "Event",
+	Message:         "Message",
+	PointOfInterest: "PointOfInterest",
+	Transaction:     "Transaction",
+	User:            "User",
 }
